fa88b3729abbd9b1174f1d19adafbdfa5e897681
Fuyu <fuyuwang2000@gmail.com>
2024-06-29 14:05:37 -0400

    Modify the docstring

--- EasyChat/server/views.py
+++ EasyChat/server/views.py
@@ -14,32 +14,43 @@ class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
     def list(self, request):
-        """List servers based on various query parameters.
+        """
+        List servers based on various query parameters.
 
         This method filters and returns a list of servers based on the 
         specified query parameters. The parameters can filter by category,
         user, server ID, and whether to include the number of members.
 
-        Args:
+        `Args`:
             request (HttpRequest): The HTTP request object containing query 
                 parameters and user information.
 
-        Query Parameters:
-            category (str): The category name to filter servers by.
-            qty (int): The quantity of servers to return.
-            by_user (bool): Whether to filter servers by the logged-in user.
-            by_serverid (str): The server ID to filter by.
-            with_num_memebers (bool): Whether to include the number of members 
-                in the response.
+        `Query Parameters`:
+        
+            - `category (str, optional)`: The category name to filter servers by. 
+                If provided, only servers belonging to this category will be returned.
 
-        Raises:
+            - `qty (int, optional)`: The maximum number of servers to return. If provided,
+                limits the number of returned servers to this quantity.
+
+            - `by_user (bool, optional)`: If set to 'true', filters servers by the logged-in user.
+                Requires the user to be authenticated.
+
+            - `by_serverid (str, optional)`: The server ID to filter by. If provided, 
+                only the server with this ID will be returned. Requires the user to be authenticated.
+
+            - `with_num_memebers (bool, optional)`: If set to 'true', includes the number of members 
+                in the server data.
+
+        `Raises`:
             AuthenticationFailed: If the user is not authenticated and attempts
                 to filter by user or server ID.
             ValidationError: If the server ID is not found or is invalid.
 
-        Returns:
+        `Returns`:
             Response: A Django REST framework Response object containing the 
-                serialized server data.
+                serialized server data, which is a list of servers matching the query parameters.
+                If `with_num_memebers` is true, each server object includes the number of members.
         """
         # Get the category id from the request
         category = request.query_params.get("category")
