656e85c01d17b310d570dd2a057b41ff3a7a43ac
Fuyu <fuyuwang2000@gmail.com>
2024-06-29 13:05:52 -0400

    Modify the serializer such that we could return related channel based on server

--- EasyChat/server/serializer.py
+++ EasyChat/server/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Category, Server
+from .models import Category, Channel, Server
 
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 class ServerSerializer(serializers.ModelSerializer):
+    # The code will also grab all channels related to the server
+    # We have a foriegn key in the model 
+    channel_server = ChannelSerializer(many=True) 
 
     class Meta:
         model = Server
