4ed6e836b6034ddc707605c127f480199ac2df9b
Fuyu <fuyuwang2000@gmail.com>
2024-06-29 23:04:11 -0400

    Creating the responsive primary app bar

--- frontend/src/pages/templates/PrimaryAppBar.tsx
+++ frontend/src/pages/templates/PrimaryAppBar.tsx
@@ -1,12 +1,36 @@
-import { AppBar, Toolbar, Link, Typography, Box, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Link, Typography, Box, IconButton, Drawer, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useState, useEffect } from "react";
 
 
 const PrimaryAppBar = () => {
+    const [sideMenu, setSideMenu] = useState(false);
     const theme = useTheme(); // Help grab the theme
+
+    // Return true when above 600 px
+    const isSmallScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
+    useEffect(() => {
+        if (isSmallScreen && sideMenu) {
+            setSideMenu(false);
+        }
+    }, [isSmallScreen]); // useEeffect will keep tracking the isSmallScreen value
+
+    const toggleDrawer =
+        (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+            if (
+                event.type === "keydown" &&
+                ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === "shift")
+            ) {
+                return;
+            }
+            setSideMenu(open);
+        }
+
     return (
         <AppBar sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 2,
             backgroundColor: theme.palette.background.default,
             borderBottom: `1px solid ${theme.palette.divider}`,
         }}>
@@ -17,10 +41,22 @@ const PrimaryAppBar = () => {
                     minHeight: theme.primaryAppBar.height,
                 }}>
                 <Box sx={{ display: { xs: "block", sm: "none" } }}>
-                    <IconButton color="inherit" aria-label="open drawer" edge="start" sx={{ mr: 2 }}>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start" sx={{ mr: 2 }}
+                        onClick={toggleDrawer(!sideMenu)}>
                         <MenuIcon />
                     </IconButton>
                 </Box>
+                <Drawer anchor='left' open={sideMenu} onClose={toggleDrawer(false)}>
+                    {[...Array(100)].map((_, i) => (
+                        <Typography key={i} paragraph>
+                            {i + 1}
+                        </Typography>
+                    ))}
+                </Drawer>
+
                 <Link href="/" underline="none" color="inherit">
                     <Typography
                         variant="h6"
