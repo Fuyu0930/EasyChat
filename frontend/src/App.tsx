b1180b28cc8978db0f8728633f6aa2f4b31814d2
Fuyu <fuyuwang2000@gmail.com>
2024-06-29 20:38:38 -0400

    React routing with react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home.tsx";
 
-  return (
-    <>
-    </>
+
+const router = createBrowserRouter(
+  // path is the url link to achieve the component
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Home />} />
+    </Route>
   )
+);
+
+// App is a React.FC type
+const App = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App
